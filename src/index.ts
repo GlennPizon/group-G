bf2e3fb3e08d755caa89f4eff69f00feee548b2a
Lenov PC <glenn.pizon2003@gmail.com>
2025-03-24 16:26:19 +0800

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,36 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
-import { AppDataSource } from "./data-source";
+import initialize, { AppDataSource } from "./data-source";
 import userRoutes from "./controllers/user.controller"; // Import user routes
 
 dotenv.config();
 const app = express();
 const port = Number(process.env.APP_PORT) || 3000;
 
-// Middleware
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
 
-// Routes
-app.use("/", userRoutes); // Registers both DELETE and POST routes
-app.use("/login", userRoutes); // Registers POST route
-app.use("/users", userRoutes); // Registers GET route
-app.get("/register", userRoutes); // Registers GET route
-app.delete("/user/:id", userRoutes); // Registers DELETE route
 
-// Start server
-AppDataSource.initialize().then(() => {
-    app.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    });
-}).catch((error) => console.log("Database connection error:", error));
+start();
+
+async function start(){
+    try{
+         // Middleware
+        app.use(express.json());
+        app.use(cors());
+        app.use(helmet());
+
+        // Routes
+        app.use("/", userRoutes); 
+        app.use("/login", userRoutes); // Registers POST route
+        app.use("/users", userRoutes); // Registers GET route
+        app.get("/register", userRoutes); // Registers GET route
+        app.delete("/user/:id", userRoutes); // Registers DELETE route
+        await initialize();
+        app.listen(port, () => {
+            console.log(`Server running at http://localhost:${port}`);
+        });
+    }catch(error){
+        console.error("Database connection error:", error);
+    }
+
+}
